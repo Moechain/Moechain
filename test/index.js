dc37f7055f8afe543b873674e66e589a215d787a
elven <elvenisboy@outlook.com>
2017-07-29 16:57:32 +0800

    use promise with leveldb

--- test/index.js
+++ test/index.js
@@ -2,8 +2,15 @@ const Utils = require('../lib/utils')
 
 const utils = new Utils()
 const key = utils.generatorKeyPair('this is secret!')
+const DB = require('../lib/db')
+const db = new DB()
 
 const c = utils.generatorSignature('Moechain is comming', key.secretKey)
 console.log('signature:' + c)
 console.log(utils.verifySignature('moechsiniscomming', c, key.publicKey))
+console.log(utils.verifySignature('Moechain is comming', c, key.publicKey))
 console.log(utils.generatorAddress(key.publicKey))
+
+db.get('233')
+.then(value => console.log(value))
+.catch((err) => console.log(err))
