2e8db2f5c94887d4ccf36a1913c6c65b02fde98d
elven <elvenisboy@outlook.com>
2017-08-30 14:32:17 +0800

    forger what i write . I am ailing

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ const BlockHeader = require('../lib/block/blockHeader')
 
 const utils = new Utils()
 const key = utils.generatorKeyPair('this is secret!')
-const header = new BlockHeader()
+const header = new BlockHeader(key)
 
 const c = utils.generatorSignature('Moechain is comming', key.secretKey)
 console.log('signature:' + c)
@@ -12,14 +12,5 @@ console.log(utils.verifySignature('Moechain is comming', c, key.publicKey))
 console.log(utils.generatorAddress(key.publicKey))
 
 console.log(header.blockTimestamp())
-
-/* const option {prefix:'ab',limit:2}
-db.find(option, function (key, value) {
-        if (key && value) {
-            //key 和 value 都存在的情况下进行逻辑处理
-            //依次返回 key:abc ,value :111  和 key:abd ,value :333 (因为我限制只读取2条记录)
-        } else {
-            //如果读到最后一个 value 将会是 undefined (这是leveldb自己实现的一种机制,所以我们可以根据 value==undefined 来判断是不是读取完成了)
-            //读取完成后做的逻辑处理
-        }
-*/
+let a = utils.generatorBlockHash({a: 233333, b: 33333332})
+console.log(a)
