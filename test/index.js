fa24a4b36fc9d7972408dbdc88c2ed47f944d793
Elven <elvenisboy@outlook.com>
2017-09-30 00:26:06 +0800

    function checkGenesis

--- test/index.js
+++ test/index.js
@@ -14,7 +14,8 @@ console.log('p k :',utils.secretKeyToPubliclKey(key.secretKey))
 
 // console.log('s k length:',key.secretKey.length)
 
-console.log('signature:' + c)
+// console.log('signature:' + c)
+console.log(utils.generatorSignature('Moechain is comming', key.secretKey).length)
 // console.log(utils.verifySignature('moechsiniscomming', c, key.publicKey))
 // console.log(utils.verifySignature('Moechain is comming', c, key.publicKey))
 // console.log(utils.generatorAddress(key.publicKey))
@@ -24,3 +25,22 @@ console.log('signature:' + c)
 // console.log('blake:',utils.blake('23333'))
 // let account = new Account(['23','384'])
 // console.log(rlp.decode(account.serialize()).toString())
+let ecrecover = function (msgHash, v, r, s) {
+    var signature = Buffer.concat([exports.setLength(r, 32), exports.setLength(s, 32)], 64)
+    var recovery = v - 27
+    if (recovery !== 0 && recovery !== 1) {
+      throw new Error('Invalid signature v value')
+    }
+    var senderPubKey = secp256k1.recover(msgHash, signature, recovery)
+    return secp256k1.publicKeyConvert(senderPubKey, false).slice(1)
+  }
+
+let  ecsign = function (msgHash, privateKey) {
+    var sig = secp256k1.sign(msgHash, privateKey)
+  
+    var ret = {}
+    ret.r = sig.signature.slice(0, 32)
+    ret.s = sig.signature.slice(32, 64)
+    ret.v = sig.recovery + 27
+    return ret
+  }
