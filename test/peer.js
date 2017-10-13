a07255c94dc6a325171ce25c3bb17fc604d7040a
Elven <elvenisboy@outlook.com>
2017-10-13 16:22:25 +0800

    test peer is OK

--- test/peer.js
+++ test/peer.js
@@ -1,45 +1,46 @@
-// var app = require('../lib/network/peers')
-// var request = require('co-supertest').agent(app.listen())
-// const expect = require('chai').expect
+const supertest = require('supertest')
+const chai = require('chai')
+const app = require('../index')
 
-// require('co-mocha')
+const expect = chai.expect
+const request = supertest(app.listen())
 
-// describe('GET /api/peer/ping', function() {
-//   it('respond with json', function(done) {
-//        request() 
-//       .get('/api/peer/ping')
-//       .set('Accept', 'application/json')
-//       .expect('Content-Type', /json/)
-//       .expect(200)
-//       .end(function(err, res) {
-//         if (err) return done(err)
-//         done()
-//       })
-//   })
+describe('GET /api/peer/ping', () => {
+  it('respond with json', done => {
+    request
+      .get('/api/peer/ping')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        done()
+      })
+  })
 
-//   it('version should be v1.0', function(done) {
-//     request(app)
-//       .get('/api/peer/ping')
-//       .set('Accept', 'application/json')
-//       .expect('Content-Type', /json/)
-//       .expect(200)
-//       .end(function(err, res) {
-//         if (err) return done(err)
-//         expect('v1.0').to.equal(res.body.version)
-//         done()
-//       })
-//   })
-// })
+  it('version should be v1.0', done => {
+    request
+      .get('/api/peer/ping')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect('v1.0').to.equal(res.body.version)
+        done()
+      })
+  })
+})
 
-// describe('POST /api/peer/connect', () => {
-//   it('content should not wrong', done => {
-//     request(app)
-//       .post('/api/peer/content')
-//       .send({ host: '127.0.0.1', port: 3000 })
-//       .end(function(err, res) {
-//         if (err) return done(err)
-//         expect(err).to.be.null
-//         done(app.close())
-//       })
-//   })
-// })
+describe('POST /api/peer/connect', () => {
+  it('content should not wrong', done => {
+    request
+      .post('/api/peer/content')
+      .send({ host: '127.0.0.1', port: 3000 })
+      .end(function(err, res) {
+        if (err) return done(err)
+        expect(err).to.be.null
+        done()
+      })
+  })
+})
