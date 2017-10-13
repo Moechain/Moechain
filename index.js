1abafa15383499ffc80ade3df7fc0bb1c863ec4d
Elven <elvenisboy@outlook.com>
2017-10-13 17:07:43 +0800

    add a getSenators route

--- index.js
+++ index.js
@@ -41,47 +41,12 @@ app.use(async (ctx, next) => {
 app.use(peer.routes(), peer.allowedMethods())
 app.use(transaction.routes(), transaction.allowedMethods())
 app.use(senators.routes(), senators.allowedMethods())
-const server = async ( ctx, next ) => {
-  let result = {
-    success: true,
-    data: null
-  }
-
-  if ( ctx.method === 'GET' ) { 
-    if ( ctx.url === '/getString.json' ) {
-      result.data = 'this is string data'
-    } else if ( ctx.url === '/getNumber.json' ) {
-      result.data = 123456
-    } else {
-      result.success = false
-    }
-    ctx.body = result
-    next && next()
-  } else if ( ctx.method === 'POST' ) {
-    if ( ctx.url === '/postData.json' ) {
-      result.data = 'ok'
-    } else {
-      result.success = false
-    }
-    ctx.body = result
-    next && next()
-  } else {
-    ctx.body = 'hello world'
-    next && next()
-  }
-}
-
-app.use(server)
 
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
 })
 
-
-
-// peerRunner.runner()
-
-
+peerRunner.runner()
 
 module.exports = app
 
